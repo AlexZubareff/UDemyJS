8c0fd96ad78ad5b8bf0013acf3990a1be0456091
Alex <azv99@yandex.ru>
2021-07-13 19:40:42 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// 
+// const res = confirm(`Ты здесь?`);
+// console.log(res);
+
+// const answer = +prompt(`Вам есть 18?`, `18`);
+// console.log(typeof(answer));
+
+// const ansvers = [];
+// ansvers[0] = prompt(`Как ваше имя?`, ``);
+// ansvers[1] = prompt(`Как ваша фамилия?`, ``);
+// ansvers[2] = +prompt(`Сколько вам лет?`, ``);
+
+
+// console.log(ansvers);
+
+let incr = 10,
+    dcr = 10;
+
+console.log(++incr);
+console.log(--dcr);
